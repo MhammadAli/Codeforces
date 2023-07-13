807c488cdee6ac9b49a26b314ba94b1e60378ce4
Muhammad Ali <66525657+MhammadAli@users.noreply.github.com>
2023-07-13 06:00:18 +0300

    Create main.cpp

---
+++ 1841A - Game with Board/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        (n == 2 || n == 3 || n == 4) ?
+        cout << "Bob\n" : cout << "Alice\n";
+
+
+    }
+    return 0;
+}
